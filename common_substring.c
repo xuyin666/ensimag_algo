35b63c172e48dcd921bee38fa3bebf11c1eeeaf5
xuyin <1499978024@qq.com>
2019-10-22 22:40:58 +0200

    modify common_substring

--- common_substring.c
+++ common_substring.c
@@ -10,35 +10,27 @@ int max (int a, int b){
     }
 }
 
-int common_substring_long(char* str,char* str1){
-    char* s;
-    char* s1;
-    int lenA=strlen(str);
-    int lenB=strlen(str1);
-    s=malloc(sizeof(char)*lenA);
-    s1=malloc(sizeof(char)*lenB);
-    strcpy(s,str);
-    strcpy(s1,str1);
-    if(strlen(s)<0 || strlen(s1)<0)
-        return -1000;
-    if(strlen(s)==0 || strlen(s1)==0)
+int common_substring_long(char* str,int lA,int lenA,char* str1,int lB,int lenB){
+    if(lA==lenA || lB==lenB)
         return 0;
-    if(s[0]==s1[0]){
-        return common_substring_long(++str,++str1)+1;
+    if(str[lA]==str1[lB]){
+        return common_substring_long(str,lA+1,lenA,str1,lB+1,lenB)+1;
     }
     else{
-        int a = common_substring_long(++str,str1);
-        int b = common_substring_long(str,++str1);
+        int a = common_substring_long(str,lA+1,lenA,str1,lB,lenB);
+        int b = common_substring_long(str,lA,lenA,str1,lB+1,lenB);
         return max(a,b);
     }
     return 0;
 }
 
 int main(int argc, char** argv){
-    printf("Hello world\n");
+    //printf("Hello world\n");
     char* str="hello,world";
     char* str1="h1lo wwwor";
+    int lenA=strlen(str);
+    int lenB=strlen(str1);
     printf("s is %s\ns1 is %s\n",str,str1);
-    int nb=common_substring_long(str,str1);
+    int nb=common_substring_long(str,0,lenA,str1,0,lenB);
     printf("the common longest substring is %d\n",nb);
 }
